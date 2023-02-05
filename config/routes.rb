957cb8cd58613163c71ca07f7a00da11a20ce345
kitajimakokoro <122000079+kitajimakokoro@users.noreply.github.com>
2023-02-05 12:32:19 +0000

    [Finish]4-14

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ root to: "homes#top"
 devise_for :users
 
 resources :post_images, only: [:new, :create, :index, :show, :destroy]
+resources :users, only: [:show, :edit]
 
 get '/homes/about' => "homes#about", as: 'about'
 end
